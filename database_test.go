2cd73ea5835a837432cf84be099cd02ab5ab9271
connero <88785126+conneroisu@users.noreply.github.com>
2024-05-17 13:46:58 -0500

    test the old implementation agaist new test

--- database_test.go
+++ database_test.go
@@ -1,8 +1,10 @@
 package dblogger
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	"os"
 	"testing"
 
 	_ "modernc.org/sqlite"
@@ -14,7 +16,7 @@ import (
 func TestQueries(t *testing.T) {
 	db, err := sql.Open("sqlite", ":memory:")
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(fmt.Errorf("failed to open database: %w", err))
 	}
 	getenv := func(key string) (string, error) {
 		switch key {
@@ -28,7 +30,12 @@ func TestQueries(t *testing.T) {
 	if err != nil {
 		tracerr.PrintSourceColor(err)
 	}
-	bytesVers := []byte(`{"level":"info","git_revision":"e8ff7107e13808170243634f7e4256245c3236be","deployment":"","go_version":"go1.22.0","build_sum":"","method":"GET","url":"/home","user_agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36","elapsed_ms":0.135385,"time":"2024-05-16T06:45:36.630788595-05:00","message":"incoming request to //home"}`)
+	// bytesVers := []byte(``)
+	// read from ./testdata/valid_log.json
+	bytesVers, err := os.ReadFile("./testdata/valid_log.json")
+	if err != nil {
+		t.Fatal(fmt.Errorf("failed to read test data: %w", err))
+	}
 	n, err := q.Write(bytesVers)
 	if err != nil {
 		t.Fatal(err)
@@ -39,11 +46,141 @@ func TestQueries(t *testing.T) {
 func TestSchema(t *testing.T) {
 	db, err := sql.Open("sqlite", ":memory:")
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(fmt.Errorf("failed to open database: %w", err))
 	}
-
 	_, err = db.Exec(SQLSchema)
+	if err != nil {
+		t.Fatal(fmt.Errorf("failed to create database: %w", err))
+	}
+}
+
+func TestTx(t *testing.T) {
+	db, err := sql.Open("sqlite", ":memory:")
+	if err != nil {
+		t.Fatal(fmt.Errorf("failed to open database: %w", err))
+	}
+	getenv := func(key string) (string, error) {
+		switch key {
+		case "DEPLOYMENT":
+			return "staging", nil
+		default:
+			return "", fmt.Errorf("invalid key: %s", key)
+		}
+	}
+	qdb, err := NewLogsDatabase(getenv, db)
 	if err != nil {
 		t.Fatal(err)
 	}
+	tx, err := db.Begin()
+	if err != nil {
+		t.Fatal(fmt.Errorf("failed to begin transaction: %w", err))
+	}
+	defer func() {
+		if err := tx.Rollback(); err != nil {
+			t.Fatal(fmt.Errorf("failed to rollback transaction: %w", err))
+		}
+	}()
+	qtx := qdb.WithTx(tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = qtx.InsertBuildSumReturningID(context.Background(), InsertBuildSumReturningIDParams{
+		BuildSum: "test",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = qtx.InsertDeploymentReturningID(context.Background(), InsertDeploymentReturningIDParams{
+		Name: "test",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = qtx.InsertGitRevisionReturningID(context.Background(), InsertGitRevisionReturningIDParams{
+		GitRevision: "test",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+}
+
+// TestQueries tests the database queries by writing a log message to the database
+func TestQueriesParallel(t *testing.T) {
+	t.Parallel()
+	db, err := sql.Open("sqlite", ":memory:")
+	if err != nil {
+		t.Fatal(fmt.Errorf("failed to open database: %w", err))
+	}
+	getenv := func(key string) (string, error) {
+		switch key {
+		case "DEPLOYMENT":
+			return "staging", nil
+		default:
+			return "", fmt.Errorf("invalid key: %s", key)
+		}
+	}
+	q, err := NewLogsDatabase(getenv, db)
+	if err != nil {
+		tracerr.PrintSourceColor(err)
+	}
+	// bytesVers := []byte(``)
+	// read from ./testdata/valid_log.json
+	bytesVers, err := os.ReadFile("./testdata/valid_log.json")
+	if err != nil {
+		t.Fatal(fmt.Errorf("failed to read test data: %w", err))
+	}
+	t.Run("write1", func(t *testing.T) {
+		t.Parallel()
+		n, err := q.Write(bytesVers)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("t: %v\n", n)
+	})
+	t.Run("write2", func(t *testing.T) {
+		t.Parallel()
+		n, err := q.Write(bytesVers)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("t: %v\n", n)
+	})
+	t.Run("write3", func(t *testing.T) {
+		t.Parallel()
+		n, err := q.Write(bytesVers)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("t: %v\n", n)
+	})
+	t.Run("write4", func(t *testing.T) {
+		t.Parallel()
+		n, err := q.Write(bytesVers)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("t: %v\n", n)
+	})
+	t.Run("write5", func(t *testing.T) {
+		t.Parallel()
+		n, err := q.Write(bytesVers)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("t: %v\n", n)
+	})
+	t.Run("write6", func(t *testing.T) {
+		t.Parallel()
+		n, err := q.Write(bytesVers)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("t: %v\n", n)
+	})
+	n, err := q.Write(bytesVers)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("t: %v\n", n)
 }
